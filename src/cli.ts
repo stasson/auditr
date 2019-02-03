50db829f1a398bf7ccf6a8c10cce56f98cd69418
stasson <sebastien.tasson@gmail.com>
2019-02-03 19:52:06 +0100

    chore: scaffold cli and test

---
+++ src/cli.ts
@@ -0,0 +1,5 @@
+import hello from './index'
+
+export function run() {
+  hello()
+}
---
+++ test/spec/config.spec.ts
@@ -0,0 +1,5 @@
+describe('config', ()=>{
+  it('default config is production mode', ()=> {
+    expect(false).toBeFalsy()
+  })
+})
